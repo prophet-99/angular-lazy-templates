987b9d0ff7cae55622b0d3711bbf57e46a626074
prophet-99 <alexanderavilab@gmail.com>
2024-09-15 13:42:55 -0500

    feat: end first version of lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 
 import { BehaviorSubject } from 'rxjs';
 
+const delayFn = (timing: number) =>
+  new Promise<void>((res) => {
+    setTimeout(() => {
+      res();
+    }, timing);
+  });
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,12 +20,14 @@ export class AppComponent {
   public flowState$ = new BehaviorSubject<FlowStateType>('NOT_STARTED');
 
   public elementOnViewport(): void {
-    this.flowState$.next('IN_PROGRESS');
+    delayFn(1000).then(() => this.flowState$.next('IN_PROGRESS'));
 
     import('./components/about/about.component').then(
       (aboutRef) => {
-        this.contentSlot.createComponent(aboutRef.AboutComponent);
-        this.flowState$.next('COMPLETE');
+        delayFn(3000).then(() => {
+          this.contentSlot.createComponent(aboutRef.AboutComponent);
+          this.flowState$.next('COMPLETE');
+        });
       },
       (err) => this.flowState$.next('FAILED')
     );
